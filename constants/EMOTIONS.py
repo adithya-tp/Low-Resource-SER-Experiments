4a84e4aed22ee566fc5a25fbda84beca6f0b121d
adithya-tp <adithyatp@yahoo.com>
2022-03-30 02:55:59 -0400

    Add filename to emotion mapping for EMOVO

--- constants/EMOTIONS.py
+++ constants/EMOTIONS.py
@@ -1,1 +1,17 @@
-AESDD_EMOTIONS = ["fear", "sadness", "happiness", "anger", "disgust"]
+AESDD_EMOTIONS = [
+    "fear", 
+    "sadness", 
+    "happiness", 
+    "anger", 
+    "disgust"
+]
+
+EMOVO_EMOTIONS = {
+    "pau": "fear", 
+    "tri": "sadness", 
+    "gio": "joy", 
+    "rab": "anger", 
+    "dis": "disgust",
+    "sor": "surprise",
+    "neu": "neutral"
+    }
--- constants/PATHS.py
+++ constants/PATHS.py
@@ -7,3 +7,4 @@ while os.path.basename(os.path.abspath(ROOT_DIR)) != GITHUB_REPO_NAME:
     ROOT_DIR = os.path.join(ROOT_DIR, "..")
 
 AESDD_DIR = os.path.join(ROOT_DIR, 'data/AESDD')
+EMOVO_DIR = os.path.join(ROOT_DIR, 'data/EMOVO')
