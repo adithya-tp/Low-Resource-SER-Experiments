f95c20af1056735429db446bf1ffdc4d931da118
adithya-tp <adithyatp@yahoo.com>
2022-04-02 19:12:15 -0400

    Update train script to check if dataloader works

--- train_wavenet.py
+++ train_wavenet.py
@@ -1,7 +1,10 @@
+from torch.utils.data import DataLoader
+from transformers import AutoConfig, Wav2Vec2FeatureExtractor
+import os
+
 import constants.EMOTIONS as EMOTS
 from audio_datasets.aessd_dataset import AESDD_Greek
-
-from transformers import AutoConfig, Wav2Vec2FeatureExtractor
+import constants.PATHS as PATHS
 
 def get_wavenet2_config(HUGGING_FACE_MODEL_PATH):
     emotions = EMOTS.AESDD_EMOTIONS
@@ -12,21 +15,32 @@ def get_wavenet2_config(HUGGING_FACE_MODEL_PATH):
         id2label=dict(zip( range(len(emotions)) , emotions )),
         finetuning_task="wav2vec2_clf",
     )
-    setattr(conf, 'pooling_mode', 'mean')
     return conf
 
 def main():
     model_name_or_path = "facebook/wav2vec2-base-100k-voxpopuli"
-    pooling_mode = "mean"
     config = get_wavenet2_config(
-        model_name_or_path
+        HUGGING_FACE_MODEL_PATH=model_name_or_path,
     )
 
     feature_extractor = \
         Wav2Vec2FeatureExtractor.from_pretrained(model_name_or_path,)
-    target_sampling_rate = feature_extractor.sampling_rate
 
-    data_loader = 
+    data_loader = DataLoader(
+        dataset=AESDD_Greek(
+            BASE_DIR=PATHS.AESDD_DIR,
+            ANNOTATIONS_PATH=os.path.join(PATHS.ROOT_DIR, 'annotations/annotations_aesdd.csv'),
+            target_sampling_rate=feature_extractor.sampling_rate
+        ),
+        batch_size=2,
+        shuffle=True,
+        collate_fn=AESDD_Greek.collate_fn
+    )
+
+    (x, lx, y) = next(iter(data_loader))
+    result = feature_extractor(x, sampling_rate=feature_extractor.sampling_rate)
+    print(f"Batch of x: {x}, \n Lengths of x: {lx}")
+    print(f"Batch of y: {y}")
 
 if __name__ == '__main__':
     main()
