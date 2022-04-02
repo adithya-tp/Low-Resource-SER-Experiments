eb0034d37ea37e68d8cd485d591feb1cdb7db3c0
adithya-tp <adithyatp@yahoo.com>
2022-04-02 19:03:56 -0400

    Add wav2vec2 skeleton

---
+++ models/wav2vec2.py
@@ -0,0 +1,9 @@
+from transformers.models.wav2vec2.modeling_wav2vec2 import Wav2Vec2PreTrainedModel, Wav2Vec2Model
+
+# Wav2vec model skeleton
+class Wav2vec2SER(Wav2Vec2PreTrainedModel):
+    def __init__(self):
+        pass
+
+    def forward(self):
+        pass
